eb30516c58df9b3f4b9d634a763414b6d1658e69
Slawek <slapyt84@gmail.com>
2023-09-21 10:42:10 +0200

    Zwraca użytkownika user lub admin i ma dostęp do odpowiednich endpointów

--- src/main/java/pl/slawek/springsecurityexample/security/MyUserDetailsService.java
+++ src/main/java/pl/slawek/springsecurityexample/security/MyUserDetailsService.java
@@ -1,26 +1,32 @@
 package pl.slawek.springsecurityexample.security;
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.List;
 
 @Service
 class MyUserDetailsService implements UserDetailsService {
 
-    private final MyBCryptPasswordEncoder passwordEncoder;
 
-    MyUserDetailsService(final MyBCryptPasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
+    private final MyUserDetailsList users;
+
+    MyUserDetailsService(final MyUserDetailsList users) {
+        this.users = users;
     }
 
     @Override
-    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-        return new User("user", passwordEncoder.encode("user"), Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+    public UserDetails loadUserByUsername(final String username) {
+        List<User> userList = users.users;
+
+        for (User user : userList) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+            }
+        throw new UsernameNotFoundException("Nie znaleziono użytkownika");
     }
 }
