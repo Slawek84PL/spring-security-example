4820da9f1817dbf1e2fb90dfbc3e467247b28e34
Slawek <slapyt84@gmail.com>
2023-09-23 14:29:16 +0200

    Adnotacja pod swaggera

--- src/main/java/pl/slawek/springsecurityexample/SpringSecurityExampleApplication.java
+++ src/main/java/pl/slawek/springsecurityexample/SpringSecurityExampleApplication.java
@@ -1,9 +1,12 @@
 package pl.slawek.springsecurityexample;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Security Example", description = "Slawek84PL", version = "1.0"))
 public class SpringSecurityExampleApplication {
 
 	public static void main(String[] args) {
